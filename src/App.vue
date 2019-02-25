a863c4c2430aceded54be2a7a76c9b6f8cd47723
yangyuan <1367511704@qq.com>
2019-02-25 16:27:02 +0800

    feat(button组件)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <base-button>button</base-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BaseButton from './components/BaseButton';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BaseButton
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+@import './style/reset.less';
 </style>
