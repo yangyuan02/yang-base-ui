6f8ba3a304a5bc4fa34536f866a884463f67b308
yangyuan <1367511704@qq.com>
2019-02-26 11:06:58 +0800

    feat(icon自己完成)

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <i class="icon" :class="'icon' + type" :style="{'color': color, 'fontSize': size + 'px'}"></i>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 14
+    },
+    color: {
+      type: String,
+      default: '#000000'
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
+
+
