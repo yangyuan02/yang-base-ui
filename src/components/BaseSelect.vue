b1d1f8052680b07b7ddc7c8ea59387a071c143d1
yangyuan <1367511704@qq.com>
2019-04-29 14:48:23 +0800

    feat(select组件更新)

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="select">
+    <div class="inner">
+      <div class="inputWrapper">
+        <input type="text" readonly placeholder="请选择" />
+        <span class="清楚"></span>
+      </div>
+      <ul class="options">
+        <li v-for="(item, index) in options" :key="index">{{item.value}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      // options数据
+      type: Array,
+      default: () => []
+    },
+    disabled: {
+      // 是否允许点击展开options
+    },
+    clearFun: {
+      // 清空事件
+    },
+    multiple: {
+      // 是否多选
+      type: Boolean,
+      default: false
+    },
+    changeFun: {
+      //选中改变的事件
+    }
+  },
+  data() {
+    return {
+      options: [{ value: '西红柿炒鸡蛋' }, { value: '青椒炒鸡蛋' }, { value: '回锅肉' }]
+    };
+  }
+};
+</script>
