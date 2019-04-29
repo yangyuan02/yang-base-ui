545ec7f575c8ab58971d67d9f9885a2216c77192
yangyuan <1367511704@qq.com>
2019-04-29 15:48:53 +0800

    feat(searchBox组件更新)

--- src/components/BaseSearchBox.vue
+++ src/components/BaseSearchBox.vue
@@ -1,1 +1,26 @@
-// 可配置的搜索框
+<template>
+  <div class="searchBox">
+    <div class="searchItem" v-for="(item, index) in conditions" :key="index">
+      <base-input v-if="item.type === 'String'" />
+      <base-select v-if="item.type === 'Select'" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseInput from './BaseInput';
+import BaseSelect from './BaseSelect';
+export default {
+  components: {
+    BaseInput,
+    BaseSelect
+  },
+  props: {
+    conditions: {
+      // searchBox表单类型
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
